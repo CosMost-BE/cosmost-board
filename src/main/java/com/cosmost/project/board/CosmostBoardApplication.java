3cc1cd060adf90ef9fa4c88b56ec0b667c58dd61
chanyoung Hong <cksdud9883@naver.com>
2022-10-17 22:10:30 +0900

    first commit

---
+++ src/main/java/com/cosmost/project/board/CosmostBoardApplication.java
@@ -0,0 +1,13 @@
+package com.cosmost.project.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmostBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmostBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cosmost/project/board/CosmostBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmost.project.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmostBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
