55b1b87982259196b79634ed15d2c1382c674964
chanyoung Hong <cksdud9883@naver.com>
2022-10-18 02:12:22 +0900

    [create] add createReport in ReportServiceImpl

--- src/main/java/com/cosmost/project/board/service/ReportServiceImpl.java
+++ src/main/java/com/cosmost/project/board/service/ReportServiceImpl.java
@@ -1,4 +1,32 @@
 package com.cosmost.project.board.service;
 
-public class ReportServiceImpl {
+import com.cosmost.project.board.infrastructure.entity.ReportCategoryEntity;
+import com.cosmost.project.board.infrastructure.entity.ReportEntity;
+import com.cosmost.project.board.infrastructure.repository.ReportCategoryEntityRepository;
+import com.cosmost.project.board.infrastructure.repository.ReportEntityRepository;
+import com.cosmost.project.board.requestbody.CreateReportCategoryRequest;
+import com.cosmost.project.board.requestbody.CreateReportRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportServiceImpl implements ReportService{
+
+    private final ReportEntityRepository reportEntityRepository;
+    private final ReportCategoryEntityRepository reportCategoryEntityRepository;
+
+    public ReportServiceImpl(ReportEntityRepository reportEntityRepository,
+                             ReportCategoryEntityRepository reportCategoryEntityRepository) {
+        this.reportEntityRepository = reportEntityRepository;
+        this.reportCategoryEntityRepository = reportCategoryEntityRepository;
+    }
+
+    @Override
+    public void createReport(CreateReportRequest createReportRequest) {
+
+        ReportEntity reportEntity = reportDtoToEntity(createReportRequest);
+        CreateReportCategoryRequest categoryRequest = new CreateReportCategoryRequest();
+        reportEntityRepository.save(reportEntity);
+        reportCategoryEntityRepository.save(categoryRequest.reportCategoryDtoToEntity(createReportRequest.getReportCategoryName(), reportEntity));
+
+    }
 }
