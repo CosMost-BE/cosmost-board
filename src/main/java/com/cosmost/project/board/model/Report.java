684718fb17a2fc89d657d960b207ef3345af81a2
chanyoung Hong <cksdud9883@naver.com>
2022-10-18 02:10:46 +0900

    [create] add field content

--- src/main/java/com/cosmost/project/board/model/Report.java
+++ src/main/java/com/cosmost/project/board/model/Report.java
@@ -1,4 +1,20 @@
 package com.cosmost.project.board.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder
 public class Report {
+
+    private Long id;
+
+    private Long reporterId;
+
+    private String reportTitle;
+
+    private String reportContent;
+
 }
