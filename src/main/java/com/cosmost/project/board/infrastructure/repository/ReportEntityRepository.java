2b1769ed56dff18f3194ba1d28ef5b20b61e8101
chanyoung Hong <cksdud9883@naver.com>
2022-10-18 02:11:48 +0900

    [create] add ReportEntityRepository

--- src/main/java/com/cosmost/project/board/infrastructure/repository/ReportEntityRepository.java
+++ src/main/java/com/cosmost/project/board/infrastructure/repository/ReportEntityRepository.java
@@ -1,4 +1,9 @@
 package com.cosmost.project.board.infrastructure.repository;
 
-public interface ReportEntityRepository {
+import com.cosmost.project.board.infrastructure.entity.ReportEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReportEntityRepository extends JpaRepository<ReportEntity, Long> {
 }
