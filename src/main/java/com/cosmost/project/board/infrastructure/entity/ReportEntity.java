fe34812c05667b604da3219677cc8fc0f6ba7def
chanyoung Hong <cksdud9883@naver.com>
2022-10-18 02:11:42 +0900

    [create] add ReportEntity

--- src/main/java/com/cosmost/project/board/infrastructure/entity/ReportEntity.java
+++ src/main/java/com/cosmost/project/board/infrastructure/entity/ReportEntity.java
@@ -1,4 +1,37 @@
 package com.cosmost.project.board.infrastructure.entity;
 
-public class ReportEntity {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "report")
+public class ReportEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private Long reporterId;
+
+    @NotNull
+    private String reportTitle;
+
+    @NotNull
+    private String reportContent;
+
+
+    @Builder
+    public ReportEntity(Long id, Long reporterId, String reportTitle, String reportContent) {
+        this.id = id;
+        this.reporterId = reporterId;
+        this.reportTitle = reportTitle;
+        this.reportContent = reportContent;
+    }
 }
