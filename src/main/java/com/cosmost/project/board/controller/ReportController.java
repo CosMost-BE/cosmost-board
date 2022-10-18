aa53b5823423132726dd837498d2eb6956b149f8
chanyoung Hong <cksdud9883@naver.com>
2022-10-18 14:01:49 +0900

    [update] add @PutMapping

--- src/main/java/com/cosmost/project/board/controller/ReportController.java
+++ src/main/java/com/cosmost/project/board/controller/ReportController.java
@@ -1,6 +1,7 @@
 package com.cosmost.project.board.controller;
 
 import com.cosmost.project.board.requestbody.CreateReportRequest;
+import com.cosmost.project.board.requestbody.UpdateReportRequest;
 import com.cosmost.project.board.service.ReportService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -32,8 +33,15 @@ public class ReportController {
     @ApiOperation(value = "신고를 등록할 때 쓰는 메소드")
     @ApiImplicitParam(name = "course", value = "코스 리뷰를 등록한 메뉴", dataType = "CourseReviewVoReq")
     @PostMapping("")
-    public ResponseEntity<String> createCourseReviews(@Valid @RequestBody CreateReportRequest request) {
+    public ResponseEntity<String> createReport(@Valid @RequestBody CreateReportRequest request) {
         reportService.createReport(request);
         return ResponseEntity.ok().body("신고가 등록 되었습니다.");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateReport(@PathVariable Long id,
+                                               @Valid @RequestBody UpdateReportRequest request){
+        reportService.updateReport(id, request);
+        return ResponseEntity.ok().body("신고가 수정 되었습니다.");
     }
 }
