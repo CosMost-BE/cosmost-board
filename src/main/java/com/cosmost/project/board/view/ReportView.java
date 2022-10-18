311693e74c019fb41ae7d3ec320f7d9ecd58afc6
chanyoung Hong <cksdud9883@naver.com>
2022-10-18 21:26:56 +0900

    [create] add View

--- src/main/java/com/cosmost/project/board/view/ReportView.java
+++ src/main/java/com/cosmost/project/board/view/ReportView.java
@@ -1,4 +1,24 @@
 package com.cosmost.project.board.view;
 
+import com.cosmost.project.board.infrastructure.entity.ReportCategoryEntity;
+import com.cosmost.project.board.model.ReportCategory;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Builder
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ReportView {
+
+    private Long id;
+    private Long reporterId;
+    private String reportTitle;
+    private String reportContent;
+
+    private List<ReportCategory> categoryEntityList;
 }
