a9f98178c7115e7d378e6b51b459a30dc8193a91
chanyoung Hong <cksdud9883@naver.com>
2022-10-19 11:09:37 +0900

    [create] add ReportCategoryListEntity

---
+++ src/main/java/com/cosmost/project/board/infrastructure/entity/ReportCategoryListEntity.java
@@ -0,0 +1,32 @@
+package com.cosmost.project.board.infrastructure.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "report_category_list")
+public class ReportCategoryListEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    ReportEntity report;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    ReportCategoryEntity reportCategory;
+
+    @Builder
+    public ReportCategoryListEntity(Long id, ReportEntity report, ReportCategoryEntity reportCategory) {
+        this.id = id;
+        this.report = report;
+        this.reportCategory = reportCategory;
+    }
+
+}
