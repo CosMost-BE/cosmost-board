3449fbb82aa310091f437d03b3c3a80d598fc415
chanyoung Hong <cksdud9883@naver.com>
2022-10-19 11:08:54 +0900

    [update] delete findByReport_Id method

--- src/main/java/com/cosmost/project/board/infrastructure/repository/ReportCategoryEntityRepository.java
+++ src/main/java/com/cosmost/project/board/infrastructure/repository/ReportCategoryEntityRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ReportCategoryEntityRepository extends JpaRepository<ReportCategoryEntity, Long> {
-    List<ReportCategoryEntity> findByReport_Id(Long id);
+
 }
