23310ec64fe7a9d099b1c2a22c51c81f2ab0f590
chanyoung Hong <cksdud9883@naver.com>
2022-11-04 14:46:30 +0900

    [update] add findByReporterIdAndId method

--- src/main/java/com/cosmost/project/board/infrastructure/repository/ReportEntityRepository.java
+++ src/main/java/com/cosmost/project/board/infrastructure/repository/ReportEntityRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ReportEntityRepository extends JpaRepository<ReportEntity, Long> {
     List<ReportEntity> findAllByReporterId(Long id);
+
+    ReportEntity findByReporterIdAndId(Long reporterId, Long id);
 }
