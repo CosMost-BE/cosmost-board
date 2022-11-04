d18c524072e64c4f1f6ec63be1452368437f4293
chanyoung Hong <cksdud9883@naver.com>
2022-11-04 20:50:50 +0900

    [update] findAllByReporterId 타입 변경

--- src/main/java/com/cosmost/project/board/infrastructure/repository/ReportEntityRepository.java
+++ src/main/java/com/cosmost/project/board/infrastructure/repository/ReportEntityRepository.java
@@ -1,14 +1,14 @@
 package com.cosmost.project.board.infrastructure.repository;
 
 import com.cosmost.project.board.infrastructure.entity.ReportEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ReportEntityRepository extends JpaRepository<ReportEntity, Long> {
-    List<ReportEntity> findAllByReporterId(Long id);
+    Slice<ReportEntity> findAllByReporterId(Long id, Pageable pageable);
 
     ReportEntity findByReporterIdAndId(Long reporterId, Long id);
 }
